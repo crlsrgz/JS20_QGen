36b38f798ff75431fe1f59feef878ce9ebab8f1b
crls <crls@fake>
2022-09-03 11:45:48 +0200

    L-21: Buttons and Loading functionality implemented

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
-// Get quotes from API
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn= document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+
+
+// Get quotes from external API
 let apiQuotes = [];
 
 function newQuote() {
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)] 
-  console.log(quote);
+  // Check if quote has an author.
+  if (!quote.author ){
+    authorText.textContent = "Unknown";
+  } else {
+    authorText.textContent = quote.author;
+  }
+  // Check length
+  if(quote.text.length > 100) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+  quoteText.textContent = quote.text;
 }
+
 async function getQuotes() {
   const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
   // const apiUrl = 'https://type.fit/api/quotes';
@@ -17,3 +38,23 @@ async function getQuotes() {
   }
 }
 getQuotes();
+
+// Load from local file
+
+// function newQuote() {
+  //   const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)] 
+  //   authorText.textContent = quote.author;
+  //   quoteText.textContent = quote.text;
+// }
+
+// newQuote();
+
+
+//Tweet quote
+function tweetQuote() {
+  const twitterURl = `https://twitter.com/intent/tweet?text=${quote.textContent} - ${authorText.textContent}`;
+  window.open(twitterURl, '_blank');
+  
+}
+newQuoteBtn.addEventListener("click",newQuote);
+twitterBtn.addEventListener("click",tweetQuote);
